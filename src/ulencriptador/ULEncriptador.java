2762799b55c44cf84b83f68916c1c5076f2d86d8
Hernan Quintana <hernan@jsatch.com>
2017-01-31 16:26:08 -0500

    Version inicial

---
+++ src/ulencriptador/ULEncriptador.java
@@ -0,0 +1,15 @@
+
+package ulencriptador;
+
+public class ULEncriptador {
+
+    public static void main(String[] args) {
+        // La aplicacion recibira los siguientes parametros (por linea
+        // de comandos):
+        // Tipo de Encriptacion: SHA1 o MD5
+        // Palabra a encriptar.
+        String tipoEncriptacion = args[0];
+        String palabraAEncriptar = args[1];
+    }
+    
+}
